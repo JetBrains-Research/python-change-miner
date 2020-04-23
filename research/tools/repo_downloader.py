f48b2ea5f1be90c1ae511bb817b264186c6db639
stspbu <stardust.skg@gmail.com>
2020-04-23 23:13:37 +0300

    Added setting research_repos_per_star

--- research/tools/repo_downloader.py
+++ research/tools/repo_downloader.py
@@ -11,6 +11,7 @@ _GITHUB_BASE_URL = 'https://api.github.com'
 _REPO_DIR = settings.get('research_repo_dir', os.getcwd())
 _REPO_CNT = settings.get('research_repo_count', 10)
 _MIN_STARS = settings.get('research_min_stars', 15)
+_REPOS_PER_STAR = settings.get('research_repos_per_star', 3)
 _QUERY_STRING = settings.get('research_repo_query_string')
 _TOKEN = settings.get('research_github_token', required=False)
 
@@ -22,6 +23,7 @@ def main():
     visited_repo_cnt = 1
     stars = _MIN_STARS
     max_repo_cnt = _REPO_CNT
+    stars_to_cnt = {}
 
     while True:
         if visited_repo_cnt > max_repo_cnt or stars < _MIN_STARS:
@@ -45,6 +47,13 @@ def main():
             if stars < _MIN_STARS:
                 logging.warning(f'Stopped, no enough stars in repo={repo_name}, stars={stars} < {_MIN_STARS}')
                 break
+
+            if not stars_to_cnt.get(stars):
+                stars_to_cnt[stars] = 0
+            stars_to_cnt[stars] += 1
+
+            if stars_to_cnt.get(stars) > _REPOS_PER_STAR:
+                continue
 
             args = ['git', 'clone', item['clone_url'], os.path.join(_REPO_DIR, repo_name)]
             p = subprocess.Popen(args, stdout=subprocess.PIPE)
