a7772cfde65b7f959b0d28689984ec334e388264
dTarasova <d.al.tarasova@gmail.com>
2020-11-14 23:37:30 +0300

    setting environment variables for both platforms added

--- main.py
+++ main.py
@@ -25,20 +25,20 @@ class RunModes:
 
     ALL = [BUILD_PY_FLOW_GRAPH, BUILD_CHANGE_GRAPH, COLLECT_CHANGE_GRAPHS, MINE_PATTERNS]
 
+def set_environment_variables(env_var, value):
+    if os.getenv(env_var) is None:
+        os.environ[env_var] = value;
 
 def main():
     PYTHON_BIN_ENV_VAR = "GUMTREE_PYTHON_BIN"
-    PYPARSER_ENV_VAR = "GUMTREE_PYPARSER_PATH"
     PYTHON_BIN_VALUE = "python3"
+    set_environment_variables(PYTHON_BIN_ENV_VAR, PYTHON_BIN_VALUE)
 
+    PYPARSER_ENV_VAR = "GUMTREE_PYPARSER_PATH"
     project_directory = os.path.dirname(os.getcwd())
-    pyparser_path = project_directory + '/external/pyparser.py'
+    pyparser_path = os.path.join(project_directory, 'external', 'pyparser.py')
+    set_environment_variables(PYPARSER_ENV_VAR, pyparser_path)
 
-    if os.getenv(PYTHON_BIN_ENV_VAR) is None:
-        os.environ[PYTHON_BIN_ENV_VAR] = PYTHON_BIN_VALUE;
-
-    if os.getenv(PYPARSER_ENV_VAR) is None:
-        os.environ[PYPARSER_ENV_VAR] = pyparser_path;
 
     logger.info('------------------------------ Starting ------------------------------')
 
