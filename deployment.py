e274cc0f58cdd80fa7378b25297592ab6fec8632
dTarasova <d.al.tarasova@gmail.com>
2020-11-25 20:49:42 +0300

    Get directory call fixed

--- deployment.py
+++ deployment.py
@@ -1,6 +1,9 @@
 import os
 
 
+PROJECT_DIRECTORY = os.path.dirname(__file__)
+
+
 def set_environment_variable(env_var, value):
     if os.getenv(env_var) is None:
         os.environ[env_var] = value
@@ -12,6 +15,5 @@ def set_all_environment_variables():
     set_environment_variable(PYTHON_BIN_ENV_VAR, PYTHON_BIN_VALUE)
 
     PYPARSER_ENV_VAR = "GUMTREE_PYPARSER_PATH"
-    project_directory = os.path.dirname(os.getcwd())
-    pyparser_path = os.path.join(project_directory, 'external', 'pythonparser_3.py')
+    pyparser_path = os.path.join(PROJECT_DIRECTORY, 'external', 'pythonparser_3.py')
     set_environment_variable(PYPARSER_ENV_VAR, pyparser_path)
