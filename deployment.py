97081a432b9ff3ef4592e891ce5311d91b5ecaac
dTarasova <d.al.tarasova@gmail.com>
2020-11-26 19:44:32 +0300

    independency from file directory used added

--- deployment.py
+++ deployment.py
@@ -1,5 +1,7 @@
 import os
 
+PROJECT_DIRECTORY = os.path.dirname(__file__)
+
 
 def set_environment_variable(env_var, value):
     if os.getenv(env_var) is None:
@@ -12,6 +14,5 @@ def set_all_environment_variables():
     set_environment_variable(PYTHON_BIN_ENV_VAR, PYTHON_BIN_VALUE)
 
     PYPARSER_ENV_VAR = "GUMTREE_PYPARSER_PATH"
-    project_directory = os.path.dirname(os.getcwd())
-    pyparser_path = os.path.join(project_directory, 'external', 'pyparser.py')
+    pyparser_path = os.path.join(PROJECT_DIRECTORY, 'external', 'pyparser.py')
     set_environment_variable(PYPARSER_ENV_VAR, pyparser_path)
